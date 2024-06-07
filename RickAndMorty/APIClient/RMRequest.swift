5dd22475276cb7f6f9f19a851b3316972f205d1f
lsj2015-hub <lsj2015@hotmail.com>
2024-06-07 13:41:55 +0800

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Sean Lee on 6/7/24.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+  
+}
